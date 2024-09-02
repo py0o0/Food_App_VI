90c561494b14b24f638be33b990140c6ebd09d5c
py0o0 <pantom0425@naver.com>
2024-09-02 17:10:22 +0900

    Delete app/src/main/java/com/example/foddapp/Weather/Constants.kt

--- app/src/main/java/com/example/foddapp/Weather/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.foddapp.Weather
-
-class Constants {
-    companion object{
-        const val BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
-        const val TAG = "tst5"
-    }
-}
