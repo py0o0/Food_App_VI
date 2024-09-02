050cd301d004fd24e82d9097e562fbb67ac00487
py0o0 <pantom0425@naver.com>
2024-09-02 17:10:12 +0900

    Delete app/src/main/java/com/example/foddapp/Weather/Apikey.kt

--- app/src/main/java/com/example/foddapp/Weather/Apikey.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foddapp.Weather
-
-class Apikey {
-    companion object{
-        const val API_KEY= "CrpOMfosu66gVr%2Bm50MKX9LO6XsXbojSzV1W42kwLs%2B71gYj5Dv429nDNzNcIUKdKqEolpIcTaBNJTzTqDzyMg%3D%3D"
-    }
-}
